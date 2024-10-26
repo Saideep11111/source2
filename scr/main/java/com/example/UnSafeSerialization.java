646ab7e8cd677fab9e6a99e02bb1319c03b4b45a
Saideep09 <98439094+Saideep09@users.noreply.github.com>
2024-10-26 01:14:06 -0500

    Update UnSafeSerialization.java

--- scr/main/java/com/example/UnSafeSerialization.java
+++ scr/main/java/com/example/UnSafeSerialization.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 
 public class UnsafeSerialization {
-    // Insecure deserialization vulnerability (Vulnerability #7)
+    // Insecure deserialization of an untrusted file
     public void deserializeObject(String filename) throws IOException, ClassNotFoundException {
         try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filename))) {
             Object obj = ois.readObject();
