b2f50f14edf3ab4c91854d2d8ce2b3ae5f878244
Saideep09 <98439094+Saideep09@users.noreply.github.com>
2024-10-26 01:56:05 -0500

    Update vulnerableApp.java

--- src/main/java/com/example/vulnerableApp.java
+++ src/main/java/com/example/vulnerableApp.java
@@ -1,7 +1,6 @@
 package com.example;
 
 public class VulnerableApp {
-
     public static void main(String[] args) {
         UserController userController = new UserController();
         userController.createUser("admin", "password123");
